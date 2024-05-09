2f62f049b278f293e38a861b7f4be499b0ea0da8
Simon Farshid <simon.farshid@outlook.com>
2024-05-08 22:44:01 -0700

    Add favicon

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./styles.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/favicon/favicon.svg"
+        />
+        <link rel="icon" type="image/png" href="/favicon/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
