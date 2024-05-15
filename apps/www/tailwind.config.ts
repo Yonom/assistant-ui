de0a211113d634a1f6e094828782e6c965ad12ce
Simon Farshid <simon.farshid@outlook.com>
2024-05-15 16:55:47 -0700

    Align container padding and width with nextra

--- apps/www/tailwind.config.ts
+++ apps/www/tailwind.config.ts
@@ -13,9 +13,9 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: "1.5rem",
       screens: {
-        "2xl": "1400px",
+        "2xl": "1440px",
       },
     },
     extend: {
