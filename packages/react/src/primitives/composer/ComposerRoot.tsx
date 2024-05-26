16f7e7af2efe231d354df25953d37b5176209dec
Simon Farshid <simon.farshid@outlook.com>
2024-05-25 20:54:58 -0700

    refactor: Remove unnecessary context and use composer.send() instead of composerForm.submit()

--- packages/react/src/primitives/composer/ComposerRoot.tsx
+++ packages/react/src/primitives/composer/ComposerRoot.tsx
@@ -6,14 +6,7 @@ import {
   type ComponentPropsWithoutRef,
   Primitive,
 } from "@radix-ui/react-primitive";
-import {
-  type FormEvent,
-  createContext,
-  forwardRef,
-  useContext,
-  useMemo,
-  useRef,
-} from "react";
+import { type FormEvent, forwardRef, useRef } from "react";
 import { useComposerContext } from "../../utils/context/useComposerContext";
 
 type ComposerRootElement = React.ElementRef<typeof Primitive.form>;
@@ -21,37 +14,12 @@ type PrimitiveFormProps = ComponentPropsWithoutRef<typeof Primitive.form>;
 
 type ComposerRootProps = PrimitiveFormProps;
 
-type ComposerContextValue = {
-  submit: () => void;
-};
-const ComposerFormContext = createContext<ComposerContextValue | null>(null);
-
-export const useComposerFormContext = () => {
-  const context = useContext(ComposerFormContext);
-  if (!context) {
-    throw new Error(
-      "Composer compound components cannot be rendered outside the Composer component",
-    );
-  }
-  return context;
-};
-
 export const ComposerRoot = forwardRef<ComposerRootElement, ComposerRootProps>(
   ({ onSubmit, ...rest }, forwardedRef) => {
     const { useComposer } = useComposerContext();
 
     const formRef = useRef<HTMLFormElement>(null);
     const ref = useComposedRefs(forwardedRef, formRef);
-
-    const composerContextValue = useMemo(
-      () => ({
-        submit: () =>
-          formRef.current?.dispatchEvent(
-            new Event("submit", { cancelable: true, bubbles: true }),
-          ),
-      }),
-      [],
-    );
 
     const handleSubmit = (e: FormEvent) => {
       const composerState = useComposer.getState();
@@ -62,13 +30,11 @@ export const ComposerRoot = forwardRef<ComposerRootElement, ComposerRootProps>(
     };
 
     return (
-      <ComposerFormContext.Provider value={composerContextValue}>
-        <Primitive.form
-          {...rest}
-          ref={ref}
-          onSubmit={composeEventHandlers(onSubmit, handleSubmit)}
-        />
-      </ComposerFormContext.Provider>
+      <Primitive.form
+        {...rest}
+        ref={ref}
+        onSubmit={composeEventHandlers(onSubmit, handleSubmit)}
+      />
     );
   },
 );
