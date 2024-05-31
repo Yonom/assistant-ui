3d6c045f221adddfee8536ced223ff8329ad2198
Simon Farshid <simon.farshid@outlook.com>
2024-05-31 15:37:32 -0700

    refactor: simplify useDummyAIAssistantContext

--- packages/react/src/adapters/vercel/useDummyAIAssistantContext.tsx
+++ packages/react/src/adapters/vercel/useDummyAIAssistantContext.tsx
@@ -1,34 +1,36 @@
 "use client";
 import { useState } from "react";
 import { create } from "zustand";
-import {
-  type AssistantStore,
-  ROOT_PARENT_ID,
-  type ThreadState,
+import type {
+  AssistantStore,
+  ThreadState,
 } from "../../utils/context/stores/AssistantTypes";
 import { makeThreadComposerStore } from "../../utils/context/stores/ComposerStore";
 import { makeViewportStore } from "../../utils/context/stores/ViewportStore";
 
+const makeDummyThreadStore = () => {
+  return create<ThreadState>(() => ({
+    messages: [],
+    isRunning: false,
+    append: async () => {
+      throw new Error("Not implemented");
+    },
+    cancelRun: () => {
+      throw new Error("Not implemented");
+    },
+    switchToBranch: () => {
+      throw new Error("Not implemented");
+    },
+    startRun: async () => {
+      throw new Error("Not implemented");
+    },
+  }));
+};
+
 export const useDummyAIAssistantContext = () => {
   const [context] = useState<AssistantStore>(() => {
-    const useThread = create<ThreadState>()(() => ({
-      messages: [],
-      isRunning: false,
-      append: async () => {
-        throw new Error("Not implemented");
-      },
-      cancelRun: () => {
-        throw new Error("Not implemented");
-      },
-      switchToBranch: () => {
-        throw new Error("Not implemented");
-      },
-      startRun: async () => {
-        throw new Error("Not implemented");
-      },
-    }));
-
     const useViewport = makeViewportStore();
+    const useThread = makeDummyThreadStore();
     const useComposer = makeThreadComposerStore(useThread);
 
     return { useThread, useViewport, useComposer };
