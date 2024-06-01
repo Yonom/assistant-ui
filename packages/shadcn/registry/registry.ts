89e8900dd55b7c793c9c280db75ecf15d7e2fa30
Simon Farshid <simon.farshid@outlook.com>
2024-06-01 08:18:48 -0700

    Add icons as dependency

--- packages/shadcn/registry/registry.ts
+++ packages/shadcn/registry/registry.ts
@@ -6,13 +6,13 @@ export const registry: RegistryIndex = [
     type: "components:ui",
     files: ["assistant-ui/assistant-modal.tsx"],
     registryDependencies: ["thread", "button", "popover", "tooltip"],
-    dependencies: ["@assistant-ui/react"],
+    dependencies: ["@assistant-ui/react", "lucide-react"],
   },
   {
     name: "thread",
     type: "components:ui",
     files: ["assistant-ui/thread.tsx"],
     registryDependencies: ["button", "avatar", "tooltip"],
-    dependencies: ["@assistant-ui/react"],
+    dependencies: ["@assistant-ui/react", "lucide-react"],
   },
 ];
