a1dddf32d2062b6a566a6b71645d1d4b17d8bc60
Simon Farshid <simon.farshid@outlook.com>
2024-06-01 17:25:39 -0700

    docs: font styling

--- apps/www/components/Home.tsx
+++ apps/www/components/Home.tsx
@@ -46,7 +46,7 @@ export default function Home() {
       <div className="mt-12 flex flex-col gap-4 self-center">
         {/* <h1 className="text-center font-mono text-4xl">assistant-ui</h1> */}
 
-        <p className="text-center font-bold text-4xl">
+        <p className="text-center font-extrabold text-4xl">
           React Components for AI Chat
         </p>
         <p className="text-center text-foreground/85 text-xl">
