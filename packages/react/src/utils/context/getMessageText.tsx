b20d8e478c4d3492a4bcf442fc4821144f8f73f7
Simon Farshid <simon.farshid@outlook.com>
2024-06-03 16:46:01 -0700

    feat: ActionBar.Copy multipart message support

---
+++ packages/react/src/utils/context/getMessageText.tsx
@@ -0,0 +1,10 @@
+"use client";
+import type { TextContentPart, ThreadMessage } from "./stores/AssistantTypes";
+
+export const getMessageText = (message: ThreadMessage) => {
+  const textParts = message.content.filter(
+    (part) => part.type === "text",
+  ) as TextContentPart[];
+
+  return textParts.map((part) => part.text).join("\n\n");
+};
