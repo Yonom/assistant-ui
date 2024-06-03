da6f196f3d13ede8ddaaf1d6af5bc60d5a175c33
Simon Farshid <simon.farshid@outlook.com>
2024-06-03 16:31:31 -0700

    fix: Update error messages for clarity (#78)

--- packages/react/src/utils/context/AssistantContext.ts
+++ packages/react/src/utils/context/AssistantContext.ts
@@ -7,7 +7,7 @@ export const useAssistantContext = () => {
   const context = useContext(AssistantContext);
   if (!context)
     throw new Error(
-      "useAssistantContext must be used within a AssistantProvider",
+      "This component must be used within a AssistantProvider",
     );
   return context;
 };
