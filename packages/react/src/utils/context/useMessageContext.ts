a0c9b07ca1fc6048952c15f43b7aaeab813df1bd
Simon Farshid <simon.farshid@outlook.com>
2024-06-05 14:09:43 -0700

    fix: Update error message in useMessageContext.ts to mention MessagePrimitive.Provider. (#97)

--- packages/react/src/utils/context/useMessageContext.ts
+++ packages/react/src/utils/context/useMessageContext.ts
@@ -6,6 +6,8 @@ export const MessageContext = createContext<MessageStore | null>(null);
 export const useMessageContext = () => {
   const context = useContext(MessageContext);
   if (!context)
-    throw new Error("This component must be used within a MessageProvider.");
+    throw new Error(
+      "This component must be used within a MessagePrimitive.Provider.",
+    );
   return context;
 };
