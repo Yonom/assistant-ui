2a870f7e44ff0e659337e64818a100b363a3a64a
Simon Farshid <simon.farshid@outlook.com>
2024-06-05 14:20:43 -0700

    feat: ContentPartPrimitive.Provider (#99)

--- packages/react/src/primitives/message/MessageContent.tsx
+++ packages/react/src/primitives/message/MessageContent.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import type { ComponentType, FC } from "react";
+import type { ComponentType, FC, ReactNode } from "react";
+import { useAssistantContext } from "../../utils/context/AssistantContext";
+import { useCombinedStore } from "../../utils/context/combined/useCombinedStore";
 import type {
   ImageContentPart,
   TextContentPart,
@@ -8,6 +10,8 @@ import type {
   UIContentPart,
 } from "../../utils/context/stores/AssistantTypes";
 import { useMessageContext } from "../../utils/context/useMessageContext";
+import { ContentPartLoadingIndicator } from "../contentPart/ContentPartLoadingIndicator";
+import { ContentPartProvider } from "../contentPart/ContentPartProvider";
 
 type MessageContentProps = {
   components?: {
@@ -38,28 +42,51 @@ export const MessageContent: FC<MessageContentProps> = ({
     tools: { by_name = {}, Fallback = defaultComponents.tools.Fallback } = {},
   } = {},
 }) => {
+  const { useThread } = useAssistantContext();
   const { useMessage } = useMessageContext();
+
+  // TODO multiple store hooks
   const content = useMessage((s) => s.message.content);
+  const isLoading = useCombinedStore(
+    [useThread, useMessage],
+    (t, s) => s.isLast && t.isRunning,
+  );
 
   return (
     <>
       {content.map((part, i) => {
         const key = i; // use the index as key, as message is generally append only
 
-        switch (part.type) {
+        const type = part.type;
+        let component: ReactNode | null = null;
+        switch (type) {
           case "text":
-            return <Text key={key} part={part} />;
+            component = <Text part={part} />;
+            break;
+
           case "image":
-            return <Image key={key} part={part} />;
+            component = <Image part={part} />;
+            break;
           case "ui":
-            return <UI key={key} part={part} />;
+            component = <UI part={part} />;
+            break;
           case "tool-call": {
             const Tool = by_name[part.name] || Fallback;
-            return <Tool key={key} part={part} />;
+            component = <Tool part={part} />;
+            break;
           }
           default:
-            return null;
+            throw new Error(`Unknown content part type: ${type}`);
         }
+
+        return (
+          <ContentPartProvider
+            key={key}
+            part={part}
+          >
+            {component}
+          </ContentPartProvider>
+        );
       })}
     </>
   );
