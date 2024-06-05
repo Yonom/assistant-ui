dd2c246df342ca83e080eb2e6ffe17ef94966aad
Simon Farshid <simon.farshid@outlook.com>
2024-06-05 14:11:16 -0700

    fix: useCombinedStore return type (#98)

--- packages/react/src/utils/context/combined/useCombinedStore.ts
+++ packages/react/src/utils/context/combined/useCombinedStore.ts
@@ -9,8 +9,8 @@ import {
 export const useCombinedStore = <T extends Array<unknown>, R>(
   stores: { [K in keyof T]: StoreApi<T[K]> },
   selector: CombinedSelector<T, R>,
-) => {
+): R => {
   // biome-ignore lint/correctness/useExhaustiveDependencies: shallow-compare the store array
-  const useCombined = useMemo(() => createCombinedStore(stores), stores);
+  const useCombined = useMemo(() => createCombinedStore<T, R>(stores), stores);
   return useCombined(selector);
 };
