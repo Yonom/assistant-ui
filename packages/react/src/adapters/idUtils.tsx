c19668379e84fc14552e1407e8596259a67a08cc
Simon Farshid <simon.farshid@outlook.com>
2024-06-07 13:12:03 -0700

    fix: Move generateId to separate file

---
+++ packages/react/src/adapters/idUtils.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { customAlphabet } from "nanoid/non-secure";
+
+export const generateId = customAlphabet(
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+  7,
+);
+
+const optimisticPrefix = "__optimistic__";
+export const generateOptimisticId = () => `${optimisticPrefix}${generateId()}`;
+export const isOptimisticId = (id: string) => id.startsWith(optimisticPrefix);
