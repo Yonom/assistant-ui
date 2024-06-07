01b4a78247766d14d179b161406291296a8ec25c
Simon Farshid <simon.farshid@outlook.com>
2024-06-07 16:53:16 -0700

    feat: VercelModelAdapter (#118)

---
+++ packages/react/src/adapters/core/local/vercel/VercelModelAdapter.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { type LanguageModel, streamText } from "ai";
+import type {
+  AssistantContentPart,
+  TextContentPart,
+} from "../../../../utils/context/stores/AssistantTypes";
+import type {
+  ChatModelAdapter,
+  ChatModelRunOptions,
+} from "../ChatModelAdapter";
+
+export class VercelModelAdapter implements ChatModelAdapter {
+  constructor(private readonly model: LanguageModel) {}
+
+  async run({ messages, abortSignal, onUpdate }: ChatModelRunOptions) {
+    const { fullStream } = await streamText({
+      model: this.model,
+      abortSignal,
+      messages: messages.map((m) => ({
+        role: m.role,
+        content: m.content.filter((c): c is TextContentPart => c.type !== "ui"),
+      })),
+    });
+
+    const content: AssistantContentPart[] = [];
+    for await (const aiPart of fullStream) {
+      switch (aiPart.type) {
+        case "text-delta": {
+          let part = content.at(-1);
+          if (!part || part.type !== "text") {
+            part = { type: "text", text: "" };
+            content.push(part);
+          }
+          part.text += aiPart.textDelta;
+          break;
+        }
+        // TODO tool results
+        case "tool-call": {
+          content.push({
+            type: "tool-call",
+            name: aiPart.toolName,
+            args: aiPart.args,
+          });
+          break;
+        }
+      }
+
+      onUpdate({ content });
+    }
+
+    return { content };
+  }
+}
