e892747eb62b63d07f130c8010d35fccb050601c
Simon Farshid <simon.farshid@outlook.com>
2024-06-13 16:25:54 -0700

    fix: mobile view of example

--- examples/with-react-hook-form/components/ui/assistant-ui/assistant-sidebar.tsx
+++ examples/with-react-hook-form/components/ui/assistant-ui/assistant-sidebar.tsx
@@ -5,10 +5,32 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { useMediaQuery } from "@react-hook/media-query";
 import type { FC, PropsWithChildren } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../tabs";
 import { Thread } from "./thread";
 
 export const AssistantSidebar: FC<PropsWithChildren> = ({ children }) => {
+  const isSmall = useMediaQuery("(max-width: 768px)");
+
+  if (isSmall) {
+    return (
+      <Tabs
+        defaultValue="app"
+        className="mx-auto h-full max-w-[480px] pt-4 flex flex-col"
+      >
+        <TabsList className="grid w-full grid-cols-2 ">
+          <TabsTrigger value="app">Form</TabsTrigger>
+          <TabsTrigger value="thread">Chat</TabsTrigger>
+        </TabsList>
+        <TabsContent value="app">{children}</TabsContent>
+        <TabsContent value="thread">
+          <Thread />
+        </TabsContent>
+      </Tabs>
+    );
+  }
+
   return (
     <ResizablePanelGroup direction="horizontal">
       <ResizablePanel>{children}</ResizablePanel>
