c6a4b6aa439ca643e4c3523f692b183d8645daa7
Simon Farshid <simon.farshid@outlook.com>
2024-06-13 18:48:47 -0700

    example: add link to website

--- examples/with-react-hook-form/app/page.tsx
+++ examples/with-react-hook-form/app/page.tsx
@@ -3,12 +3,14 @@
 import { SignupForm } from "@/components/SignupForm";
 import { AssistantSidebar } from "@/components/ui/assistant-ui/assistant-sidebar";
 import { Form } from "@/components/ui/form";
-import { useAssistantForm, useAssistantInstructions } from "@assistant-ui/react-hook-form";
+import {
+  useAssistantForm,
+  useAssistantInstructions,
+} from "@assistant-ui/react-hook-form";
+import Link from "next/link";
 
 export default function Home() {
-  useAssistantInstructions(
-    "Help users sign up for Simon's hackathon."
-  )
+  useAssistantInstructions("Help users sign up for Simon's hackathon.");
   const form = useAssistantForm({
     defaultValues: {
       firstName: "",
@@ -21,19 +23,32 @@ export default function Home() {
   });
 
   return (
-    <AssistantSidebar>
-      <div className="h-full overflow-y-scroll">
-        <main className="container py-8">
-          <h1 className="mb-2 font-extrabold text-2xl">Simon's Hackathon</h1>
-          <p>
-            I'm hosting a Hackathon on AI UX. Be the first to get an invite!
-          </p>
+    <>
+      <AssistantSidebar>
+        <div className="h-full overflow-y-scroll">
+          <main className="container py-8">
+            <h1 className="mb-2 font-semibold text-2xl">Simon's Hackathon</h1>
+            <p>
+              I'm hosting a Hackathon on AI UX. Be the first to get an invite!
+            </p>
 
-          <Form {...form}>
-            <SignupForm />
-          </Form>
-        </main>
-      </div>
-    </AssistantSidebar>
+            <div className="my-4 font-bold">
+              Built with{" "}
+              <Link
+                href="https://github.com/Yonom/assistant-ui"
+                className="text-blue-600 underline"
+              >
+                Assistant UI
+              </Link>
+              .
+            </div>
+
+            <Form {...form}>
+              <SignupForm />
+            </Form>
+          </main>
+        </div>
+      </AssistantSidebar>
+    </>
   );
 }
