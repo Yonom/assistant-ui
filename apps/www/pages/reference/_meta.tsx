e200b69cbe1b42e28cc31a94d4300766d7a39fdb
Simon Farshid <simon.farshid@outlook.com>
2024-06-19 09:20:27 -0700

    docs: Add reference docs for react hook form

--- apps/www/pages/reference/_meta.tsx
+++ apps/www/pages/reference/_meta.tsx
@@ -1,4 +1,11 @@
 export default {
+  "-- integrations": {
+    title: "Integrations",
+    type: "separator",
+  },
+  integrations: {
+    display: "children",
+  },
   "-- primitives": {
     title: "Primitives",
     type: "separator",
