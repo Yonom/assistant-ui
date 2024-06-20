00c2ad2872f571bd4e3871e4ce8936d63b41d29b
Simon Farshid <simon.farshid@outlook.com>
2024-06-19 23:45:28 -0700

    docs: add parameters table component and document react-hook-form parameters

--- apps/www/components/docs/index.ts
+++ apps/www/components/docs/index.ts
@@ -1,3 +1,4 @@
 export { PropsTable } from "./PropsTable";
 export { KeyboardTable } from "./KeyboardTable";
 export { DataAttributesTable } from "./DataAttributesTable";
+export { ParametersTable } from "./ParametersTable";
