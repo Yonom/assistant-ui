e354c5de0b961b51df0c225f68883df5ba2b11e8
Simon Farshid <simon.farshid@outlook.com>
2024-06-22 15:51:18 -0700

    feat: use tailwind-transformer in react-ui

--- packages/react-ui/src/index.ts
+++ packages/react-ui/src/index.ts
@@ -1,3 +1,1 @@
-import "./global.css";
-
 export { Thread } from "./thread";
--- packages/react-ui/tailwind.config.ts
+++ packages/react-ui/tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 
 const config = {
   content: ["./src/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-  // prefix: "assistant-ui-",
   corePlugins: {
     preflight: false,
   },
@@ -16,52 +15,52 @@ const config = {
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        border: "hsl(var(--aui-border))",
+        input: "hsl(var(--aui-input))",
+        ring: "hsl(var(--aui-ring))",
+        background: "hsl(var(--aui-background))",
+        foreground: "hsl(var(--aui-foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: "hsl(var(--aui-primary))",
+          foreground: "hsl(var(--aui-primary-foreground))",
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: "hsl(var(--aui-secondary))",
+          foreground: "hsl(var(--aui-secondary-foreground))",
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: "hsl(var(--aui-destructive))",
+          foreground: "hsl(var(--aui-destructive-foreground))",
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: "hsl(var(--aui-muted))",
+          foreground: "hsl(var(--aui-muted-foreground))",
         },
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: "hsl(var(--aui-accent))",
+          foreground: "hsl(var(--aui-accent-foreground))",
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: "hsl(var(--aui-popover))",
+          foreground: "hsl(var(--aui-popover-foreground))",
         },
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: "hsl(var(--aui-card))",
+          foreground: "hsl(var(--aui-card-foreground))",
         },
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: "var(--aui-radius)",
+        md: "calc(var(--aui-radius) - 2px)",
+        sm: "calc(var(--aui-radius) - 4px)",
       },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "var(--aui-radix-accordion-content-height)" },
         },
         "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
+          from: { height: "var(--aui-radix-accordion-content-height)" },
           to: { height: "0" },
         },
       },
