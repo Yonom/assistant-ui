0889512fdc4650e9ce8fc432fa8585acfda9c29a
Simon Farshid <simon.farshid@outlook.com>
2024-06-24 20:11:07 -0700

    feat: add useThreadEmpty primitive hook

--- packages/react/src/primitive-hooks/thread/index.ts
+++ packages/react/src/primitive-hooks/thread/index.ts
@@ -1,3 +1,4 @@
+export { useThreadEmpty } from "./useThreadEmpty";
 export { useThreadIf } from "./useThreadIf";
 export { useThreadScrollToBottom } from "./useThreadScrollToBottom";
 export { useThreadSuggestion } from "./useThreadSuggestion";
