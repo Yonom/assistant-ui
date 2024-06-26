84e538a0740cdd9af2807f5809b52bb78d355b1b
Simon Farshid <simon.farshid@outlook.com>
2024-06-25 23:22:54 -0700

    fix: use sideOffset in assistant-modal template

--- apps/www/components/modal/ModalChat.tsx
+++ apps/www/components/modal/ModalChat.tsx
@@ -2,8 +2,9 @@ import { AssistantModal } from "@/components/ui/assistant-ui/assistant-modal";
 
 export const ModalChat = () => {
   return (
-    <div className="relative h-full w-full">
+    <div className="flex h-full w-full items-center justify-center p-4">
       <AssistantModal />
+      <p className="bold text-lg">The Assistant Modal is available in the bottom right corner of the screen.</p>
     </div>
   );
 };
