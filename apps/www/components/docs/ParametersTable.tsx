6292a4be2d6bd602127206eac7bcfc7bf18106b8
Simon Farshid <simon.farshid@outlook.com>
2024-06-28 00:07:33 -0700

    fix: actionbar hover mode disappear bug

--- apps/www/components/docs/ParametersTable.tsx
+++ apps/www/components/docs/ParametersTable.tsx
@@ -89,7 +89,7 @@ const ParametersBox: FC<ParametersTableProps> = ({ type, parameters }) => {
   return (
     <div className="relative m-2 mb-1 flex flex-col rounded-lg border">
       {!!type && (
-        <h3 className="bg-background text-foreground/70 absolute right-3 z-10 -translate-y-1/2 rounded-md border px-4 py-2 font-mono text-xs font-semibold">
+        <h3 className="bg-background text-foreground/70 absolute right-3 z-50 -translate-y-1/2 rounded-md border px-4 py-2 font-mono text-xs font-semibold">
           {type}
         </h3>
       )}
