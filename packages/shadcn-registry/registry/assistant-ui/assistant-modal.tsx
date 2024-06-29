a5ae9be03153298f445678642044cccfb19b3c3c
Simon Farshid <simon.farshid@outlook.com>
2024-06-28 23:07:06 -0700

    feat: Modal content expand from button effect

--- packages/shadcn-registry/registry/assistant-ui/assistant-modal.tsx
+++ packages/shadcn-registry/registry/assistant-ui/assistant-modal.tsx
@@ -17,13 +17,15 @@ import { AssistantModalPrimitive } from "@assistant-ui/react";
 export const AssistantModal: FC = () => {
   return (
     <AssistantModalPrimitive.Root>
-      <AssistantModalPrimitive.Trigger asChild>
-        <FloatingAssistantButton />
-      </AssistantModalPrimitive.Trigger>
+      <AssistantModalPrimitive.Anchor className="fixed bottom-4 right-4 size-12">
+        <AssistantModalPrimitive.Trigger asChild>
+          <FloatingAssistantButton />
+        </AssistantModalPrimitive.Trigger>
+      </AssistantModalPrimitive.Anchor>
       <AssistantModalPrimitive.Content
         sideOffset={16}
         className={
-          "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 slide-in-from-bottom-2 bg-popover text-popover-foreground data-[state=closed]:animate-out data-[state=open]:animate-in z-50 h-[500px] w-[400px] rounded-xl border p-0 shadow-md outline-none"
+          "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out data-[state=open]:zoom-in data-[state=open]:slide-in-from-bottom-1/2 data-[state=open]:slide-in-from-right-1/2 data-[state=closed]:slide-out-to-bottom-1/2 data-[state=closed]:slide-out-to-right-1/2 bg-popover text-popover-foreground data-[state=closed]:animate-out data-[state=open]:animate-in z-50 h-[500px] w-[400px] rounded-xl border p-0 shadow-md outline-none"
         }
       >
         <Thread />
@@ -48,7 +50,7 @@ const FloatingAssistantButton = forwardRef<
             variant="default"
             size="icon"
             {...rest}
-            className="fixed bottom-4 right-4 size-12 rounded-full shadow transition-transform hover:scale-110 active:scale-90"
+            className="size-full rounded-full shadow transition-transform hover:scale-110 active:scale-90"
             ref={ref}
           >
             <BotIcon
