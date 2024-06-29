c373fc93c2008f74cb754ce0cc07ffbb7ef8ce3a
Simon Farshid <simon.farshid@outlook.com>
2024-06-28 23:06:28 -0700

    feat: AssistantModalPrimitive.Anchor (#361)

--- packages/react/src/primitives/assistantModal/index.ts
+++ packages/react/src/primitives/assistantModal/index.ts
@@ -1,3 +1,4 @@
 export { AssistantModalPrimitiveRoot as Root } from "./AssistantModalRoot";
 export { AssistantModalPrimitiveTrigger as Trigger } from "./AssistantModalTrigger";
 export { AssistantModalPrimitiveContent as Content } from "./AssistantModalContent";
+export { AssistantModalPrimitiveAnchor as Anchor } from "./AssistantModalAnchor";
