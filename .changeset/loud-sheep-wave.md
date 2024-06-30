5a8644a18371f088677a0281f511568334f18b9b
Simon Farshid <simon.farshid@outlook.com>
2024-06-30 13:17:03 -0700

    fix: better message hover state tracking

---
+++ .changeset/loud-sheep-wave.md
@@ -0,0 +1,5 @@
+---
+"@assistant-ui/react": patch
+---
+
+fix: better message hover state tracking
--- packages/react/src/primitives/message/MessageRoot.tsx
+++ packages/react/src/primitives/message/MessageRoot.tsx
@@ -1,37 +1,58 @@
 "use client";
 
-import { composeEventHandlers } from "@radix-ui/primitive";
 import { Primitive } from "@radix-ui/react-primitive";
-import { type ElementRef, forwardRef, ComponentPropsWithoutRef } from "react";
+import {
+  type ElementRef,
+  forwardRef,
+  ComponentPropsWithoutRef,
+  useCallback,
+} from "react";
 import { useMessageContext } from "../../context/react/MessageContext";
+import { useManagedRef } from "../../utils/hooks/useManagedRef";
+import { useComposedRefs } from "@radix-ui/react-compose-refs";
 
 type MessagePrimitiveRootElement = ElementRef<typeof Primitive.div>;
 type PrimitiveDivProps = ComponentPropsWithoutRef<typeof Primitive.div>;
+
+const useIsHoveringRef = () => {
+  const { useMessageUtils } = useMessageContext();
+
+  const callbackRef = useCallback(
+    (el: HTMLElement) => {
+      const setIsHovering = useMessageUtils.getState().setIsHovering;
+
+      const handleMouseEnter = () => {
+        setIsHovering(true);
+      };
+      const handleMouseLeave = () => {
+        setIsHovering(false);
+      };
+
+      el.addEventListener("mouseenter", handleMouseEnter);
+      el.addEventListener("mouseleave", handleMouseLeave);
+
+      return () => {
+        el.removeEventListener("mouseenter", handleMouseEnter);
+        el.removeEventListener("mouseleave", handleMouseLeave);
+        setIsHovering(false);
+      };
+    },
+    [useMessageUtils],
+  );
+
+  return useManagedRef(callbackRef);
+};
 
 export type MessagePrimitiveRootProps = PrimitiveDivProps;
 
 export const MessagePrimitiveRoot = forwardRef<
   MessagePrimitiveRootElement,
   MessagePrimitiveRootProps
->(({ onMouseEnter, onMouseLeave, ...rest }, ref) => {
-  const { useMessageUtils } = useMessageContext();
-  const setIsHovering = useMessageUtils((s) => s.setIsHovering);
+>(({ onMouseEnter, onMouseLeave, ...rest }, forwardRef) => {
+  const isHoveringRef = useIsHoveringRef();
+  const ref = useComposedRefs<HTMLDivElement>(forwardRef, isHoveringRef);
 
-  const handleMouseEnter = () => {
-    setIsHovering(true);
-  };
-  const handleMouseLeave = () => {
-    setIsHovering(false);
-  };
-
-  return (
-    <Primitive.div
-      {...rest}
-      ref={ref}
-      onMouseEnter={composeEventHandlers(onMouseEnter, handleMouseEnter)}
-      onMouseLeave={composeEventHandlers(onMouseLeave, handleMouseLeave)}
-    />
-  );
+  return <Primitive.div {...rest} ref={ref} />;
 });
 
 MessagePrimitiveRoot.displayName = "MessagePrimitive.Root";
