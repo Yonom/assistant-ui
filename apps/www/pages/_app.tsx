f4fdec90f7cf6922ab590857b4e0af36779d0737
Simon Farshid <simon.farshid@outlook.com>
2024-07-02 20:53:39 -0700

    fix: imports

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -8,7 +8,7 @@ import "./styles.css";
 import "@radix-ui/themes/styles.css";
 import "@assistant-ui/react-ui/styles.css";
 import "@assistant-ui/react-ui/markdown-styles.css";
-import "@assistant-ui/react-ui/theme/default.css";
+import "@assistant-ui/react-ui/themes/default.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
