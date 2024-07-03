2ca13810819776b937803cca7af6a3781ed70030
Simon Farshid <simon.farshid@outlook.com>
2024-07-02 20:51:01 -0700

    fix: use theme (#384)

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -8,6 +8,7 @@ import "./styles.css";
 import "@radix-ui/themes/styles.css";
 import "@assistant-ui/react-ui/styles.css";
 import "@assistant-ui/react-ui/markdown-styles.css";
+import "@assistant-ui/react-ui/theme/default.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
