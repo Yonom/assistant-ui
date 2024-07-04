6d1f6e7012cc40f4eb5c5ca5bb4622b9bcdec363
Simon Farshid <simon.farshid@outlook.com>
2024-07-04 13:56:36 -0700

    refactor: react-ui prefix color names

--- packages/react-ui/tailwind.config.ts
+++ packages/react-ui/tailwind.config.ts
@@ -42,39 +42,41 @@ const config = {
     },
     extend: {
       colors: {
-        //   border: "hsl(var(--aui-border))",
-        input: "hsl(var(--aui-input))",
-        ring: "hsl(var(--aui-ring))",
-        background: "hsl(var(--aui-background))",
-        foreground: "hsl(var(--aui-foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--aui-primary))",
-          foreground: "hsl(var(--aui-primary-foreground))",
+        aui: {
+          //   border: "hsl(var(--aui-border))",
+          input: "hsl(var(--aui-input))",
+          ring: "hsl(var(--aui-ring))",
+          background: "hsl(var(--aui-background))",
+          foreground: "hsl(var(--aui-foreground))",
+          primary: {
+            DEFAULT: "hsl(var(--aui-primary))",
+            foreground: "hsl(var(--aui-primary-foreground))",
+          },
+          //   secondary: {
+          //     DEFAULT: "hsl(var(--aui-secondary))",
+          //     foreground: "hsl(var(--aui-secondary-foreground))",
+          //   },
+          //   destructive: {
+          //     DEFAULT: "hsl(var(--aui-destructive))",
+          //     foreground: "hsl(var(--aui-destructive-foreground))",
+          //   },
+          muted: {
+            DEFAULT: "hsl(var(--aui-muted))",
+            foreground: "hsl(var(--aui-muted-foreground))",
+          },
+          accent: {
+            DEFAULT: "hsl(var(--aui-accent))",
+            foreground: "hsl(var(--aui-accent-foreground))",
+          },
+          popover: {
+            DEFAULT: "hsl(var(--aui-popover))",
+            foreground: "hsl(var(--aui-popover-foreground))",
+          },
+          //   card: {
+          //     DEFAULT: "hsl(var(--aui-card))",
+          //     foreground: "hsl(var(--aui-card-foreground))",
+          //   },
         },
-        //   secondary: {
-        //     DEFAULT: "hsl(var(--aui-secondary))",
-        //     foreground: "hsl(var(--aui-secondary-foreground))",
-        //   },
-        //   destructive: {
-        //     DEFAULT: "hsl(var(--aui-destructive))",
-        //     foreground: "hsl(var(--aui-destructive-foreground))",
-        //   },
-        muted: {
-          DEFAULT: "hsl(var(--aui-muted))",
-          foreground: "hsl(var(--aui-muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--aui-accent))",
-          foreground: "hsl(var(--aui-accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--aui-popover))",
-          foreground: "hsl(var(--aui-popover-foreground))",
-        },
-        //   card: {
-        //     DEFAULT: "hsl(var(--aui-card))",
-        //     foreground: "hsl(var(--aui-card-foreground))",
-        //   },
       },
       borderRadius: {
         lg: "var(--aui-radius)",
