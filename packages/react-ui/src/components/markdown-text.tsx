8381f7eb325a3d40b176bd1a4a29cb71335d3213
Simon Farshid <simon.farshid@outlook.com>
2024-07-04 21:02:08 -0700

    feat: makeMarkdownText API

--- packages/react-ui/src/components/markdown-text.tsx
+++ packages/react-ui/src/components/markdown-text.tsx
@@ -1,29 +1,27 @@
-"use client";
-
 import { MarkdownTextPrimitive } from "@assistant-ui/react-markdown";
-import remarkGfm from "remark-gfm";
-import { FC, memo } from "react";
+import { FC } from "react";
 import { MarkdownTextPrimitiveProps } from "@assistant-ui/react-markdown";
 import { TextContentPartProps } from "@assistant-ui/react";
 
-// TODO probably make a builder function for this
-const MarkdownTextImpl: FC<
-  Partial<MarkdownTextPrimitiveProps> & TextContentPartProps
-> = ({ remarkPlugins, className, part, status, ...rest }) => {
-  return (
-    <MarkdownTextPrimitive
-      className={
-        "aui-md-root" +
-        (status === "in_progress" ? " aui-md aui-md-in-progress" : "") +
-        (!!className ? " " + className : "")
-      }
-      remarkPlugins={[remarkGfm, ...(remarkPlugins ?? [])]}
-      smooth // TODO figure out the default for this
-      {...rest}
-    />
-  );
+type MarkdownTextProps = Partial<MarkdownTextPrimitiveProps>;
+
+export const makeMarkdownText = ({
+  className,
+  ...rest
+}: MarkdownTextProps = {}) => {
+  const MarkdownTextImpl: FC<TextContentPartProps> = ({ status }) => {
+    return (
+      <MarkdownTextPrimitive
+        className={
+          "aui-md-root" +
+          (status === "in_progress" ? " aui-md aui-md-in-progress" : "") +
+          (!!className ? " " + className : "")
+        }
+        {...rest}
+      />
+    );
+  };
+  MarkdownTextImpl.displayName = "MarkdownText";
+
+  return MarkdownTextImpl;
 };
-
-MarkdownTextImpl.displayName = "MarkdownText";
-
-export const MarkdownText = memo(MarkdownTextImpl);
