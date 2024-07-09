c4ccfcbb0da8a042d1169fd615f53aaf2f7f1883
Simon Farshid <simon.farshid@outlook.com>
2024-07-09 09:14:45 -0700

    fix: update next config on docs (#430)

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -3,6 +3,22 @@ import createMDX from "fumadocs-mdx/config";
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+  transpilePackages: ["@assistant-ui/*"],
+  rewrites: async () => ({
+    beforeFiles: [
+      {
+        source: "/umami/:path*",
+        destination: "https://assistant-ui-umami.vercel.app/:path*",
+      },
+    ],
+    fallback: [
+      {
+        source: "/registry/:path*",
+        destination: "https://ui.shadcn.com/registry/:path*",
+      },
+    ],
+  }),
+};
 
 export default withMDX(config);
