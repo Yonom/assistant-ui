112c445d32f9d6550e47fa3fbc236b7cacdb15a7
Simon Farshid <simon.farshid@outlook.com>
2024-07-09 17:02:27 -0700

    feat: react-ui tailwindcss integration

--- packages/tailwindcss-transformer/src/index.ts
+++ packages/tailwindcss-transformer/src/index.ts
@@ -1,164 +1,40 @@
-import { createHash } from "node:crypto";
+import { type Declaration, Plugin } from "postcss";
+import valueParser, { type ParsedValue } from "postcss-value-parser";
 
-import postcss from "postcss";
-import * as recast from "recast";
-import * as tsParser from "recast/parsers/babel-ts.js";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-import { replaceVariableScope } from "./replace-variable-scope";
-
-/**
- * A map of hashed classnames from Tailwind CSS classes and their original values
- */
-type StyleCache = Map<string, string>;
-
-const clRegex = /^aui-[a-z0-9-]+$/;
-
-function isBinaryExpression(node: recast.types.namedTypes.BinaryExpression) {
-  return recast.types.namedTypes.BinaryExpression.check(node);
-}
-
-function isLogicalExpression(node: recast.types.namedTypes.LogicalExpression) {
-  return recast.types.namedTypes.LogicalExpression.check(node);
-}
-
-function isRightmostOperand(path: any) {
-  let parentPath = path.parentPath;
-  while (isLogicalExpression(parentPath.node)) {
-    if (parentPath.node.right !== path.node) {
-      return false;
-    }
-    parentPath = parentPath.parentPath;
-  }
-  return true;
-}
-
-function generateHashedClassName(value: string) {
-  return (
-    "aui-" +
-    createHash("sha256").update(value, "utf8").digest("hex").slice(0, 8)
-  );
-}
-
-function visitNode(
-  node: recast.types.ASTNode,
-  ctx: { styleCache: StyleCache },
-  visitors?: recast.types.Visitor,
-) {
-  recast.visit(node, {
-    visitStringLiteral(path) {
-      if (clRegex.test(path.node.value)) {
-        return false;
+const tailwindcssAui = (): Plugin => ({
+  postcssPlugin: "@assistant-ui/tailwindcss-transformer",
+  AtRule: {
+    keyframes: (atRule) => {
+      if (!atRule.params.startsWith("aui-")) {
+        atRule.params = `aui-${atRule.params}`;
       }
-      if (isBinaryExpression(path.parentPath.node)) {
-        return false;
-      }
-      if (
-        isLogicalExpression(path.parentPath.node) &&
-        !isRightmostOperand(path)
-      ) {
-        return false;
-      }
-      if (
-        path.parentPath.node.type === "ObjectProperty" &&
-        path.parentPath.node.key === path.node
-      ) {
-        return false;
-      }
-
-      const cn = generateHashedClassName(path.node.value);
-      ctx.styleCache.set(cn, path.node.value);
-      path.node.value = cn;
-      return false;
     },
-    ...visitors,
-  });
-}
-
-export function transform(code: string, ctx: { styleCache: StyleCache }) {
-  const ast = recast.parse(code, { parser: tsParser });
-
-  recast.visit(ast, {
-    // visit className attributes containing TW classes
-    visitJSXAttribute(path) {
-      const node = path.node;
-      if (node.name.name === "className") {
-        visitNode(node, ctx, {
-          // Stop traversal if we encounter a function call
-          // cn/cx/clsx/cva are handled by the `visitCallExpression` visitor
-          visitCallExpression() {
-            return false;
-          },
+  },
+  Declaration(decl: Declaration) {
+    if (decl.prop.startsWith("--tw-")) {
+      decl.prop = decl.prop.replace("--tw-", "--aui-");
+    }
+    const value: ParsedValue = valueParser(decl.value);
+    value.walk((node) => {
+      if (node.type === "function" && node.value === "var") {
+        node.nodes.forEach((n) => {
+          if (n.type === "word" && n.value.startsWith("--tw-")) {
+            n.value = n.value.replace("--tw-", "--aui-");
+          }
         });
       }
-      this.traverse(path);
-    },
-    // visit a `className` property within any object containing TW classes
-    visitObjectProperty(path) {
-      const node = path.node;
-      if (
-        path.node.key.type === "Identifier" &&
-        path.node.key.name === "className"
-      ) {
-        visitNode(node, ctx);
-      }
-      this.traverse(path);
-    },
-    // visit function calls containing TW classes
-    visitCallExpression(path) {
-      const node = path.node;
-      // `className` concatenation functions
-      if (
-        node.callee.type === "Identifier" &&
-        ["cn", "cx", "clsx"].includes(node.callee.name)
-      ) {
-        visitNode(node, ctx);
-      }
-      // cva functions (note: only compatible with cva@1.0)
-      if (
-        node.callee.type === "Identifier" &&
-        node.callee.name === "cva" &&
-        node.arguments[0]?.type === "ObjectExpression"
-      ) {
-        for (const property of node.arguments[0].properties) {
-          if (
-            property.type === "ObjectProperty" &&
-            property.key.type === "Identifier" &&
-            ["base", "variants"].includes(property.key.name)
-          ) {
-            visitNode(property, ctx);
-          }
-        }
-      }
-      this.traverse(path);
-    },
-  });
+    });
+    decl.value = value.toString();
 
-  return recast.print(ast).code;
-}
+    if (
+      (decl.prop === "animation" || decl.prop === "animation-name") &&
+      !decl.value.startsWith("aui-")
+    ) {
+      decl.value = `aui-${decl.value}`;
+    }
+  },
+});
 
-export async function generateStylesheet(
-  styleCache: StyleCache,
-  ctx: { globalCss: string },
-) {
-  let stylesheet = "@tailwind base;\n\n";
+tailwindcssAui.postcss = true;
 
-  if (ctx.globalCss) {
-    stylesheet += ctx.globalCss + "\n";
-  }
-
-  for (const [cn, value] of styleCache) {
-    stylesheet += `.${cn} { @apply ${value} }\n`;
-  }
-
-  const result = await postcss([
-    tailwindcss(),
-    replaceVariableScope,
-    autoprefixer(),
-  ]).process(stylesheet, {
-    from: "styles.css",
-  });
-
-  return result.css.replace(/\n\n/g, "\n");
-}
+export = tailwindcssAui;
