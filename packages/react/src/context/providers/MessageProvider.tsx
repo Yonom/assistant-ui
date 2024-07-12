d106515ffde5ab3b62a11bbb927b96bc418526be
Simon Farshid <simon.farshid@outlook.com>
2024-07-12 08:37:08 -0700

    fix: type usage (#476)

--- packages/react/src/context/providers/MessageProvider.tsx
+++ packages/react/src/context/providers/MessageProvider.tsx
@@ -3,7 +3,7 @@
 import { type FC, type PropsWithChildren, useEffect, useState } from "react";
 import { StoreApi, create } from "zustand";
 import type {
-  AppendUserContentPart,
+  CoreUserContentPart,
   ThreadMessage,
 } from "../../types/AssistantTypes";
 import { getMessageText } from "../../utils/getMessageText";
@@ -81,7 +81,7 @@ const useMessageContext = (messageIndex: number) => {
           );
 
         const nonTextParts = message.content.filter(
-          (part): part is AppendUserContentPart =>
+          (part): part is CoreUserContentPart =>
             part.type !== "text" && part.type !== "ui",
         );
         useThreadActions.getState().append({
