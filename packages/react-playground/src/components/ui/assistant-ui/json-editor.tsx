7aaab4bd1c02b2191c34e794e3da0b55a54fc530
Simon Farshid <simon.farshid@outlook.com>
2024-07-14 21:59:35 -0700

    fix: simple-code-editor esbuild compat

--- packages/react-playground/src/components/ui/assistant-ui/json-editor.tsx
+++ packages/react-playground/src/components/ui/assistant-ui/json-editor.tsx
@@ -1,9 +1,11 @@
 import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs";
-import "prismjs/components/prism-json";
-import "prismjs/themes/prism.css";
 import { FC } from "react";
 import { cn } from "../../../lib/utils";
+
+// https://github.com/react-simple-code-editor/react-simple-code-editor/issues/106
+const EditorComponent =
+  (Editor as unknown as { default: typeof Editor }).default ?? Editor;
 
 type JSONEditorProps = {
   placeholder?: string;
@@ -14,7 +16,7 @@ type JSONEditorProps = {
 
 export const JSONEditor: FC<JSONEditorProps> = ({ className, ...props }) => {
   return (
-    <Editor
+    <EditorComponent
       {...props}
       highlight={(code) => highlight(code, languages["json"]!, "json")}
       className={cn("w-full font-mono [&>textarea]:outline-none", className)}
