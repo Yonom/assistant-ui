5f923a36a2131218cda507535f7b0d22d8e41d0a
Simon Farshid <simon.farshid@outlook.com>
2024-07-18 21:34:36 -0700

    docs: getting started docs revamp (#533)

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -1,5 +1,104 @@
-import Home from "@/components/Home";
+"use client";
+
+import { Claude } from "@/components/claude/Claude";
+import { Shadcn } from "@/components/shadcn/Shadcn";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { useChat } from "ai/react";
+import { AssistantRuntimeProvider, useEdgeRuntime } from "@assistant-ui/react";
+import Link from "next/link";
+import { useState } from "react";
+import { ChatGPT } from "../../components/chatgpt/ChatGPT";
+import { GenUI } from "../../components/genui/GenUI";
+import { Artifacts } from "../../components/artifacts/Artifacts";
+import { ModalChat } from "../../components/modal/ModalChat";
+
+const supportedModels = [
+  {
+    name: "Standalone",
+    component: Shadcn,
+  },
+  {
+    name: "Modal",
+    component: ModalChat,
+  },
+  {
+    name: "Tool UI",
+    component: GenUI,
+  },
+  {
+    name: "Artifacts",
+    component: Artifacts,
+  },
+  {
+    name: "ChatGPT Theme",
+    component: ChatGPT,
+  },
+  {
+    name: "Claude Theme",
+    component: Claude,
+  },
+];
 
 export default function HomePage() {
-  return <Home />;
+  const [selectedModel, setSelectedModel] = useState(supportedModels[0]!);
+  const ChatComponent = selectedModel.component;
+
+  return (
+    <main className="container mx-auto flex flex-col gap-6 self-stretch py-4">
+      <div className="mt-12 flex flex-col gap-4 self-center">
+        <h1 className="text-center text-4xl font-extrabold">
+          Build in-app AI chatbots
+          <br />
+          in days, not weeks.
+        </h1>
+        <p className="text-foreground/85 text-center text-xl">
+          assistant-ui is a chatbot UI for your React app
+        </p>
+      </div>
+
+      <div className="mb-8 flex justify-center gap-2">
+        <Button asChild>
+          <Link href="/docs">Get Started</Link>
+        </Button>
+      </div>
+      <div className="mx-auto flex w-full max-w-screen-xl flex-col">
+        <p className="font-bold">Examples:</p>
+        <div className="mt-2 flex overflow-x-scroll">
+          <div className="flex flex-grow gap-3">
+            {supportedModels.map((model) => (
+              <Badge
+                key={model.name}
+                onClick={() => setSelectedModel(model)}
+                className="shrink-0 cursor-pointer px-4 py-2"
+                variant={
+                  selectedModel.name === model.name ? "default" : "secondary"
+                }
+              >
+                {model.name}
+              </Badge>
+            ))}
+          </div>
+        </div>
+        <div className="mt-4 h-[650px] overflow-hidden rounded-lg border shadow">
+          <MyRuntimeProvider>
+            <ChatComponent />
+          </MyRuntimeProvider>
+        </div>
+      </div>
+    </main>
+  );
 }
+
+export type AssistantProps = {
+  chat: ReturnType<typeof useChat>;
+};
+
+const MyRuntimeProvider = ({ children }: { children: React.ReactNode }) => {
+  const runtime = useEdgeRuntime({ api: "/api/chat" });
+  return (
+    <AssistantRuntimeProvider runtime={runtime}>
+      {children}
+    </AssistantRuntimeProvider>
+  );
+};
