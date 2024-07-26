bdb8e4a5694a3cd24610dfa87d1fb8c075d833d6
Simon Farshid <simon.farshid@outlook.com>
2024-07-26 16:52:33 -0700

    feat(markdown): useIsMarkdownCodeBlock hook

--- packages/react-markdown/src/overrides/PreOverride.tsx
+++ packages/react-markdown/src/overrides/PreOverride.tsx
@@ -1,10 +1,14 @@
-import { createContext, ComponentPropsWithoutRef } from "react";
+import { createContext, ComponentPropsWithoutRef, useContext } from "react";
 import { PreComponent } from "./types";
 
 export const PreContext = createContext<Omit<
   ComponentPropsWithoutRef<PreComponent>,
   "children"
 > | null>(null);
+
+export const useIsMarkdownCodeBlock = () => {
+  return useContext(PreContext) !== null;
+};
 
 export const PreOverride: PreComponent = ({ children, ...rest }) => {
   return <PreContext.Provider value={rest}>{children}</PreContext.Provider>;
