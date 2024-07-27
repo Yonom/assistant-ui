2241f0261e731e361a22318de9dc49159c1cd958
Simon Farshid <simon.farshid@outlook.com>
2024-07-27 15:31:40 -0700

    docs: add testimonials on the homepage (#595)

---
+++ apps/docs/components/testimonials/testimonials.ts
@@ -0,0 +1,91 @@
+export type Testimonial = {
+  platform: "X";
+  avatar: any;
+  username: string;
+  message: string;
+  url: string;
+};
+
+export const TESTIMONIALS: Testimonial[] = [
+  {
+    platform: "X",
+    avatar: require("./profiles/iVBtd9hs_400x400.jpg"),
+    username: "@raw_works",
+    message: "i needed this 7 months ago!",
+    url: "https://x.com/raw_works/status/1797111840188809472",
+  },
+  {
+    platform: "X",
+    avatar: require("./profiles/g2f-gI5N_400x400.jpg"),
+    username: "@manuuonly",
+    message:
+      "was just thinking of researching a tool that could help me do this.. awesome, great product!",
+    url: "https://x.com/manuuonly/status/1797511225523454243",
+  },
+  {
+    platform: "X",
+    avatar: require("./profiles/_-WcCpRw_400x400.jpg"),
+    username: "@paRRthiv",
+    message: "Amazing 🤩\n\nAnd generative UI is a super amazing 😍",
+    url: "https://x.com/paRRthiv/status/1797629569366929538",
+  },
+  {
+    platform: "X",
+    avatar: require("./profiles/vnGL1NHF_400x400.jpg"),
+    username: "@eliasdevs",
+    message:
+      "I can't highlight how simple @simonfarshid makes it to add generative chat to react apps all while still using @vercel AI SDK. I was able to get it into production in 2 hours and users can now chat with ai about their resumes!",
+    url: "https://x.com/eliasdevs/status/1800691268194013219",
+  },
+  {
+    platform: "X",
+    avatar: require("./profiles/0nZ-ergb_400x400.jpg"),
+    username: "@kyritzb",
+    message: "THANK YOU :)\n\nDUDE WAS SO QUICK NOW I CAN KEEP BUILDING",
+    url: "https://x.com/kyritzb/status/1802884178276753585",
+  },
+  {
+    platform: "X",
+    avatar: require("./profiles/F3t4GW4t_400x400.jpg"),
+    username: "@rahulchhabra07",
+    message: "So cool ❤️",
+    url: "https://x.com/rahulchhabra07/status/1797088346805870984",
+  },
+  {
+    platform: "X",
+    avatar: require("./profiles/8rrHE4fe_400x400.jpg"),
+    username: "@AGIHouseSF",
+    message:
+      "Great tool for your next hackathon: 5 different UIs for chat, all ready to go!",
+    url: "https://x.com/AGIHouseSF/status/1797111970363322796",
+  },
+  {
+    platform: "X",
+    avatar: require("./profiles/uuRXSsia_400x400.jpg"),
+    username: "@gardening_gnome",
+    message: "Quite awesome. Want to learn more about the generative UI",
+    url: "https://x.com/gardening_gnome/status/1797112443786924348",
+  },
+  {
+    platform: "X",
+    avatar: require("./profiles/_5AVrIjJ_400x400.jpg"),
+    username: "@aditya_advani",
+    message: "Chat UX for your next React Chat App. Perfect project!",
+    url: "https://x.com/aditya_advani/status/1797111877828583754",
+  },
+  {
+    platform: "X",
+    avatar: require("./profiles/Y7hXHG3i_400x400.jpg"),
+    username: "@Masmoudi_Salim",
+    message: "Clean feeling",
+    url: "https://x.com/Masmoudi_Salim/status/1797111633212530695",
+  },
+  {
+    platform: "X",
+    avatar: require("./profiles/Mc0m3zkD_400x400.jpg"),
+    username: "@simonfarshid",
+    message:
+      "Go check out http://assistant-ui.com if you need a chatbot UI with magic baked in ✨",
+    url: "https://x.com/simonfarshid/status/1801482963886182555",
+  },
+];
