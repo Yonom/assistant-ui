069df85acbeed9eb32f0eaa83369aea2550ac61b
Simon Farshid <simon.farshid@outlook.com>
2024-08-06 23:28:49 -0700

    docs: q&a message pairs

--- apps/docs/app/api/entelligence/route.ts
+++ apps/docs/app/api/entelligence/route.ts
@@ -7,16 +7,24 @@ export const POST = async (req: Request) => {
   const { content: question, role } = messages.pop()!;
   if (role !== "user" || !question) throw new Error("No question provided");
 
+  const history = messages.reduce(
+    (pairs, msg, i, arr) => {
+      const next = arr[i + 1];
+      if (msg.role === "user" && next?.role === "assistant") {
+        pairs.push([msg.content, next.content]);
+      }
+      return pairs;
+    },
+    [] as [string, string][],
+  );
+
   return fetch(process.env["ENTELLIGENCE_API_URL"]!, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      history: messages.map((m) => ({
-        role: m.role,
-        content: m.content,
-      })),
+      history,
       question,
       vectorDBUrl: "Yonom&assistant-ui",
       advancedAgent: false,
