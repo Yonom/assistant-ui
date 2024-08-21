8f4fe7d6f3966a2c267fbfa6192c1d8138736cba
Simon Farshid <simon.farshid@outlook.com>
2024-08-20 22:45:00 -0700

    fix: CircleStopIcon invisible on safari

--- packages/react/src/ui/base/CircleStopIcon.tsx
+++ packages/react/src/ui/base/CircleStopIcon.tsx
@@ -6,6 +6,8 @@ export const CircleStopIcon: FC = () => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 16 16"
       fill="currentColor"
+      width="16"
+      height="16"
     >
       <rect width="10" height="10" x="3" y="3" rx="2" />
     </svg>
