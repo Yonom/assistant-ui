78095848452496f23cc6e8ca24e810d7430c5bdf
Simon Farshid <simon.farshid@outlook.com>
2024-09-07 15:43:55 -0700

    feat(ai-sdk): attachments support (#764)

--- .changeset/olive-olives-learn.md
+++ .changeset/olive-olives-learn.md
@@ -1,5 +1,7 @@
 ---
 "@assistant-ui/react": patch
+"@assistant-ui/react-playground": patch
+"@assistant-ui/react-ai-sdk": patch
 ---
 
 feat: add composer attachments state
---
+++ packages/react-ai-sdk/src/ui/utils/toCreateMessage.ts
@@ -0,0 +1,44 @@
+import { AppendMessage } from "@assistant-ui/react";
+import { CreateMessage } from "ai";
+
+export const toCreateMessage = async (
+  message: AppendMessage,
+): Promise<CreateMessage> => {
+  const content = message.content
+    .filter((part) => part.type === "text")
+    .map((t) => t.text)
+    .join("\n\n");
+
+  const images = message.content
+    .filter((part) => part.type === "image")
+    .map((part) => ({ url: part.image }));
+
+  return {
+    role: message.role,
+    content,
+    experimental_attachments: [
+      ...images,
+      ...(await Promise.all(
+        message.attachments.map(async (m) => {
+          if (m.file == null)
+            throw new Error("Attachment did not contain a file");
+          return {
+            contentType: m.file.type,
+            name: m.file.name,
+            url: await getFileDataURL(m.file),
+          };
+        }),
+      )),
+    ],
+  };
+};
+
+const getFileDataURL = (file: File) =>
+  new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+
+    reader.readAsDataURL(file);
+  });
