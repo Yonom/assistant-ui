2642be4ba43870b2df835d4c78d4cdc18fbe56b0
Simon Farshid <simon.farshid@outlook.com>
2024-09-07 16:41:27 -0700

    feat: ComposerPrimitive.AddAttachment

--- packages/react/src/primitives/composer/index.ts
+++ packages/react/src/primitives/composer/index.ts
@@ -2,5 +2,6 @@ export { ComposerPrimitiveRoot as Root } from "./ComposerRoot";
 export { ComposerPrimitiveInput as Input } from "./ComposerInput";
 export { ComposerPrimitiveSend as Send } from "./ComposerSend";
 export { ComposerPrimitiveCancel as Cancel } from "./ComposerCancel";
+export { ComposerPrimitiveAddAttachment as AddAttachment } from "./ComposerAddAttachment";
 export { ComposerPrimitiveAttachments as Attachments } from "./ComposerAttachments";
 export { ComposerPrimitiveIf as If } from "./ComposerIf";
