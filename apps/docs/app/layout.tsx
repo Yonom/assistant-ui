c8680d5715abe041be5bef9466fa35060d0d9bf5
Simon Farshid <simon.farshid@outlook.com>
2024-09-18 14:43:11 -0700

    docs: add vector (#855)

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -3,6 +3,7 @@ import { RootProvider } from "fumadocs-ui/provider";
 import type { ReactNode } from "react";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import Script from "next/script";
 
 export const metadata = {
   title: {
@@ -27,6 +28,15 @@ export default function Layout({ children }: { children: ReactNode }) {
           data-website-id="6f07c001-46a2-411f-9241-4f7f5afb60ee"
           data-domains="www.assistant-ui.com"
         ></script>
+
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+        !function(e,r){try{if(e.vector)return void console.log("Vector snippet included more than once.");var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){console.error("Error loading Vector:",e)}}(window,document);
+        vector.load("d9af9bfb-c10c-4eed-9366-57cdc0a97ee9");
+    `,
+          }}
+        />
       </body>
     </html>
   );
