6507071c03f0288ad6f1794c8d9b2d8071f076bc
Simon Farshid <simon.farshid@outlook.com>
2024-09-19 14:56:35 -0700

    feat: TextContentPartProvider isRunning (#861)
    and fix streaming support

---
+++ .changeset/bright-ladybugs-brake.md
@@ -0,0 +1,5 @@
+---
+"@assistant-ui/react": patch
+---
+
+fix: TextContentPartProvider text streaming support
--- packages/react/src/context/providers/TextContentPartProvider.tsx
+++ packages/react/src/context/providers/TextContentPartProvider.tsx
@@ -1,24 +1,35 @@
 "use client";
 
 import type { FC, PropsWithChildren } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ContentPartState } from "../stores";
 import { create } from "zustand";
 import {
   ContentPartContext,
   ContentPartContextValue,
 } from "../react/ContentPartContext";
+import { ContentPartStatus, TextContentPart } from "../../types/AssistantTypes";
+import { writableStore } from "../ReadonlyStore";
 
 type TextContentPartProviderProps = {
   text: string;
+  isRunning?: boolean | undefined;
+};
+
+const COMPLETE_STATUS: ContentPartStatus = {
+  type: "complete",
+};
+
+const RUNNING_STATUS: ContentPartStatus = {
+  type: "running",
 };
 
 export const TextContentPartProvider: FC<
   PropsWithChildren<TextContentPartProviderProps>
-> = ({ children, text }) => {
+> = ({ children, text, isRunning }) => {
   const [context] = useState<ContentPartContextValue>(() => {
     const useContentPart = create<ContentPartState>(() => ({
-      status: { type: "complete" },
+      status: isRunning ? RUNNING_STATUS : COMPLETE_STATUS,
       part: { type: "text", text },
     }));
 
@@ -26,6 +37,26 @@ export const TextContentPartProvider: FC<
       useContentPart,
     };
   });
+
+  useEffect(() => {
+    const state = context.useContentPart.getState();
+    const textUpdated = (state.part as TextContentPart).text !== text;
+    const targetTextPart = textUpdated
+      ? { type: "text" as const, text }
+      : state.part;
+    const targetStatus = isRunning ? RUNNING_STATUS : COMPLETE_STATUS;
+    const statusUpdated = state.status !== targetStatus;
+
+    if (!textUpdated && !statusUpdated) return;
+
+    writableStore(context.useContentPart).setState(
+      {
+        part: targetTextPart,
+        status: targetStatus,
+      },
+      true,
+    );
+  }, [context, isRunning, text]);
 
   return (
     <ContentPartContext.Provider value={context}>
