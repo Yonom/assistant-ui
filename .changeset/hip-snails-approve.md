0a8202e00d25146671c23f18bf8fc49de48eb942
Simon Farshid <simon.farshid@outlook.com>
2024-09-28 19:48:05 -0700

    fix: tool UI result can arrive before assistant message is marked as complete (#918)

---
+++ .changeset/hip-snails-approve.md
@@ -0,0 +1,5 @@
+---
+"@assistant-ui/react-langgraph": patch
+---
+
+fix: tool UI result can arrive before assistant message is marked as complete
--- packages/react-langgraph/src/useLangGraphMessages.ts
+++ packages/react-langgraph/src/useLangGraphMessages.ts
@@ -1,4 +1,7 @@
 import { useState, useCallback } from "react";
+import { INTERNAL } from "@assistant-ui/react";
+
+const { generateId } = INTERNAL;
 
 export const useLangGraphMessages = <TMessage>({
   stream,
@@ -25,6 +28,11 @@ export const useLangGraphMessages = <TMessage>({
       let partialMessages: Map<string, TMessage> = new Map();
       for await (const chunk of response) {
         if (chunk.event === "messages/partial") {
+          // bug fix: messages/complete is not sent for some python langchain backends
+          if (completeMessages.length === 0) {
+            completeMessages.push(...optimisticMessages);
+          }
+
           for (const message of chunk.data) {
             if (!message.id) throw new Error("Partial message missing id");
 
@@ -32,27 +40,30 @@ export const useLangGraphMessages = <TMessage>({
           }
         } else if (chunk.event === "messages/complete") {
           for (const message of chunk.data) {
-            if (!message.id) continue;
-            partialMessages.delete(message.id);
-          }
+            if (message.id) {
+              partialMessages.delete(message.id);
+            }
 
-          completeMessages.push(...chunk.data);
+            // bug fix: tool results can arrive before the message is complete ?
+            // if we have partial messages left, we add complete message to partial messages
+            if (partialMessages.size > 0) {
+              partialMessages.set(message.id ?? generateId(), message);
+            } else {
+              completeMessages.push(message);
+            }
+          }
         } else {
           continue;
         }
 
-        const prevMessages = completeMessages.length
-          ? completeMessages
-          : optimisticMessages;
-
-        setMessages([...prevMessages, ...partialMessages.values()]);
+        setMessages([...completeMessages, ...partialMessages.values()]);
       }
-      if (partialMessages.size > 0) {
-        throw new Error("A partial message was not marked as complete");
-      }
+      // if (partialMessages.size > 0) {
+      //   throw new Error("A partial message was not marked as complete");
+      // }
     },
     [stream],
   );
-
+  console.log("messages", messages);
   return { messages, sendMessage, setMessages };
 };
