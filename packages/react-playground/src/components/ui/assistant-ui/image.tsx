b80d30fb5df8c1baf73afeec68c6b2fc0f119776
Simon Farshid <simon.farshid@outlook.com>
2024-09-30 20:29:31 -0700

    refactor: deprecated part usage (#935)

--- packages/react-playground/src/components/ui/assistant-ui/image.tsx
+++ packages/react-playground/src/components/ui/assistant-ui/image.tsx
@@ -1,11 +1,11 @@
 import { ImageContentPartComponent } from "@assistant-ui/react";
 import { RemoveContentPartButton } from "./remove-content-part";
 
-export const Image: ImageContentPartComponent = ({ part }) => {
+export const Image: ImageContentPartComponent = ({ image }) => {
   return (
     <div className="flex justify-between gap-2">
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={part.image} alt="Image" className="size-20 object-contain" />
+      <img src={image} alt="Image" className="size-20 object-contain" />
 
       <RemoveContentPartButton />
     </div>
