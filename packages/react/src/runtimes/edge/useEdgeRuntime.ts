18e3f7bb988c8101dff0630d8f5308aaca221f37
Simon Farshid <simon.farshid@outlook.com>
2024-10-04 13:17:31 -0700

    fix: useDangerousInBrowserRuntime correct options forwarding

--- packages/react/src/runtimes/edge/useEdgeRuntime.ts
+++ packages/react/src/runtimes/edge/useEdgeRuntime.ts
@@ -1,19 +1,13 @@
 import { LocalRuntimeOptions, useLocalRuntime } from "..";
 import { useState } from "react";
 import { EdgeChatAdapterOptions, EdgeChatAdapter } from "./EdgeChatAdapter";
+import { splitLocalRuntimeOptions } from "../local/LocalRuntimeOptions";
 
 export type EdgeRuntimeOptions = EdgeChatAdapterOptions & LocalRuntimeOptions;
 
-export const useEdgeRuntime = ({
-  initialMessages,
-  maxToolRoundtrips,
-  adapters,
-  ...options
-}: EdgeRuntimeOptions) => {
-  const [adapter] = useState(() => new EdgeChatAdapter(options));
-  return useLocalRuntime(adapter, {
-    initialMessages,
-    maxToolRoundtrips,
-    adapters,
-  });
+export const useEdgeRuntime = (options: EdgeRuntimeOptions) => {
+  const { localRuntimeOptions, otherOptions } =
+    splitLocalRuntimeOptions(options);
+  const [adapter] = useState(() => new EdgeChatAdapter(otherOptions));
+  return useLocalRuntime(adapter, localRuntimeOptions);
 };
