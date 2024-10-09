ae423c5319799655442544618785e40397c80a3d
Ever Alvarez <51029456+EverStarck@users.noreply.github.com>
2024-10-09 12:49:54 -0600

    fix: add `/getting-started` to docs links

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -68,7 +68,7 @@ export default function HomePage() {
 
       <div className="mb-8 flex justify-center gap-2">
         <Button asChild>
-          <Link href="/docs">Get Started</Link>
+          <Link href="/docs/getting-started">Get Started</Link>
         </Button>
       </div>
       <div className="mx-auto flex w-full max-w-screen-xl flex-col">
