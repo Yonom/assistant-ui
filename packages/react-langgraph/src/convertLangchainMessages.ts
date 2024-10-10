ce93e7346f0593026190a310d57f32444f0995c8
Simon Farshid <simon.farshid@outlook.com>
2024-10-10 11:15:43 -0700

    feat(langgraph): handle MessageContentComplex types (#964)

--- packages/react-langgraph/src/convertLangchainMessages.ts
+++ packages/react-langgraph/src/convertLangchainMessages.ts
@@ -3,6 +3,7 @@
 import { useExternalMessageConverter } from "@assistant-ui/react";
 import { LangChainMessage } from "./types";
 import { ToolCallContentPart } from "@assistant-ui/react";
+import { ThreadUserMessage } from "@assistant-ui/react";
 
 export const convertLangchainMessages: useExternalMessageConverter.Callback<
   LangChainMessage
@@ -15,10 +16,42 @@ export const convertLangchainMessages: useExternalMessageConverter.Callback<
         content: [{ type: "text", text: message.content }],
       };
     case "human":
+      let content: ThreadUserMessage["content"];
+
+      if (typeof message.content === "string") {
+        content = [{ type: "text" as const, text: message.content }];
+      } else {
+        content = message.content.map((part) => {
+          if (typeof part === "string") {
+            return { type: "text", text: part };
+          } else {
+            const type = part.type;
+            switch (type) {
+              case "text":
+                return { type: "text" as const, text: part.text };
+
+              case "image_url":
+                return {
+                  type: "image" as const,
+                  image:
+                    typeof part.image_url === "string"
+                      ? part.image_url
+                      : part.image_url.url,
+                };
+              default:
+                const unhandledType: never = type;
+                throw new Error(
+                  `Unhandled content part type: ${unhandledType}`,
+                );
+            }
+          }
+        });
+      }
+
       return {
         role: "user",
         id: message.id,
-        content: [{ type: "text", text: message.content }],
+        content,
       };
     case "ai":
       return {
