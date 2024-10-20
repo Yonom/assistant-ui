9e46d6cd5a0eb7ee55a430f4e79e331079f62e15
Simon Farshid <simon.farshid@outlook.com>
2024-10-20 11:35:13 -0700

    fix: do not cache adapter in useEdgeRuntime (#1034)
    fixes #1077
    
    Co-authored-by: xiangst <280304286@163.com>

--- packages/react/src/runtimes/edge/useEdgeRuntime.ts
+++ packages/react/src/runtimes/edge/useEdgeRuntime.ts
@@ -1,5 +1,4 @@
 import { LocalRuntimeOptions, useLocalRuntime } from "..";
-import { useState } from "react";
 import { EdgeChatAdapterOptions, EdgeChatAdapter } from "./EdgeChatAdapter";
 import { splitLocalRuntimeOptions } from "../local/LocalRuntimeOptions";
 
@@ -8,6 +7,9 @@ export type EdgeRuntimeOptions = EdgeChatAdapterOptions & LocalRuntimeOptions;
 export const useEdgeRuntime = (options: EdgeRuntimeOptions) => {
   const { localRuntimeOptions, otherOptions } =
     splitLocalRuntimeOptions(options);
-  const [adapter] = useState(() => new EdgeChatAdapter(otherOptions));
-  return useLocalRuntime(adapter, localRuntimeOptions);
+
+  return useLocalRuntime(
+    new EdgeChatAdapter(otherOptions),
+    localRuntimeOptions,
+  );
 };
