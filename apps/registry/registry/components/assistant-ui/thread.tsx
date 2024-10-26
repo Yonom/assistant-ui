b39501dc6d6c022854acbc918bd930f156809c41
Simon Farshid <simon.farshid@outlook.com>
2024-10-26 01:07:57 -0700

    move to website

--- apps/registry/registry/components/assistant-ui/thread.tsx
+++ apps/registry/registry/components/assistant-ui/thread.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { useEdgeRuntime, Thread } from "@assistant-ui/react";
+import { makeMarkdownText } from "@assistant-ui/react-markdown";
+
+const MarkdownText = makeMarkdownText();
 
 export const MyAssistant = () => {
   const runtime = useEdgeRuntime({
     api: "/api/chat",
   });
 
-  return <Thread runtime={runtime} />;
+  return (
+    <Thread
+      runtime={runtime}
+      assistantMessage={{ components: { Text: MarkdownText } }}
+    />
+  );
 };
