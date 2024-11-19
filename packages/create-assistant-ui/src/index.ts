9b0b33a6037a98e25cd17e70a4787091533e86ee
Simon Farshid <simon.farshid@outlook.com>
2024-11-19 14:08:23 -0800

    cli: npm create assistant-ui

---
+++ packages/create-assistant-ui/src/index.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { create } from "../../cli/src/commands/create";
+
+process.on("SIGINT", () => process.exit(0));
+process.on("SIGTERM", () => process.exit(0));
+
+async function main() {
+  create.parse();
+}
+
+main();
