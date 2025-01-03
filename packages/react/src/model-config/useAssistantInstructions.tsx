6a17ec29a2feb91fa31b2380409b74c55a3bf2e1
Simon Farshid <simon.farshid@outlook.com>
2025-01-03 12:57:47 -0800

    feat: useAssistantInstructions disable support (#1300)

--- packages/react/src/model-config/useAssistantInstructions.tsx
+++ packages/react/src/model-config/useAssistantInstructions.tsx
@@ -3,9 +3,27 @@
 import { useEffect } from "react";
 import { useAssistantRuntime } from "../context";
 
-export const useAssistantInstructions = (instruction: string) => {
+type AssistantInstructionsConfig = {
+  disabled?: boolean | undefined;
+  instruction: string;
+};
+
+const getInstructions = (
+  instruction: string | AssistantInstructionsConfig,
+): AssistantInstructionsConfig => {
+  if (typeof instruction === "string") return { instruction };
+  return instruction;
+};
+
+export const useAssistantInstructions = (
+  config: string | AssistantInstructionsConfig,
+) => {
+  const { instruction, disabled = false } = getInstructions(config);
   const assistantRuntime = useAssistantRuntime();
+
   useEffect(() => {
+    if (disabled) return;
+
     const config = {
       system: instruction,
     };
