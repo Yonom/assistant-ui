ca043b093de49190404d2115f36d14cc733b8e9b
Simon Farshid <simon.farshid@outlook.com>
2025-01-11 00:02:09 -0800

    examples: add usage example of cloud threadlist (#1344)

--- examples/with-langgraph/app/layout.tsx
+++ examples/with-langgraph/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Montserrat } from "next/font/google";
 import { MyRuntimeProvider } from "./MyRuntimeProvider";
+import { Suspense } from "react";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({
     <MyRuntimeProvider>
       <html lang="en">
         <body className={cn(montserrat.className, "h-dvh")}>
-          {children}
+          <Suspense>{children}</Suspense>
         </body>
       </html>
     </MyRuntimeProvider>
--- examples/with-langgraph/app/page.tsx
+++ examples/with-langgraph/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Thread } from "@assistant-ui/react";
+import { Thread, ThreadList } from "@assistant-ui/react";
 import { PriceSnapshotTool } from "@/components/tools/price-snapshot/PriceSnapshotTool";
 import { PurchaseStockTool } from "@/components/tools/purchase-stock/PurchaseStockTool";
 import { ToolFallback } from "@/components/tools/ToolFallback";
@@ -10,24 +10,31 @@ const MarkdownText = makeMarkdownText({});
 
 export default function Home() {
   return (
-    <div className="flex h-full flex-col">
-      <Thread
-        welcome={{
-          suggestions: [
-            {
-              prompt: "How much revenue did Apple make last year?",
-            },
-            {
-              prompt: "Is McDonald's profitable?",
-            },
-            {
-              prompt: "What's the current stock price of Tesla?",
-            },
-          ],
-        }}
-        assistantMessage={{ components: { Text: MarkdownText, ToolFallback } }}
-        tools={[PriceSnapshotTool, PurchaseStockTool]}
-      />
+    <div className="flex h-full">
+      <div className="max-w-md">
+        <ThreadList />
+      </div>
+      <div className="flex-grow">
+        <Thread
+          welcome={{
+            suggestions: [
+              {
+                prompt: "How much revenue did Apple make last year?",
+              },
+              {
+                prompt: "Is McDonald's profitable?",
+              },
+              {
+                prompt: "What's the current stock price of Tesla?",
+              },
+            ],
+          }}
+          assistantMessage={{
+            components: { Text: MarkdownText, ToolFallback },
+          }}
+          tools={[PriceSnapshotTool, PurchaseStockTool]}
+        />
+      </div>
     </div>
   );
 }
