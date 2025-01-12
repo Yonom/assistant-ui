4f3834af300d7a06d5ea5ac24ab86775df7384fd
Simon Farshid <simon.farshid@outlook.com>
2025-01-12 12:53:30 -0800

    refactor: deprecate UI content part (#1353)

--- packages/react-ai-sdk/src/rsc/useVercelRSCRuntime.tsx
+++ packages/react-ai-sdk/src/rsc/useVercelRSCRuntime.tsx
@@ -3,10 +3,38 @@
 import type { VercelRSCAdapter } from "./VercelRSCAdapter";
 import {
   ExternalStoreAdapter,
+  getExternalStoreMessage,
+  TextContentPartComponent,
   ThreadMessageLike,
+  useExternalMessageConverter,
   useExternalStoreRuntime,
+  useMessage,
+  useThread,
 } from "@assistant-ui/react";
 import { VercelRSCMessage } from "./VercelRSCMessage";
+import { useCallback, useMemo } from "react";
+
+const symbolInternalRSCExtras = Symbol("internal-rsc-extras");
+
+type RSCThreadExtras =
+  | {
+      [symbolInternalRSCExtras]?: {
+        convertFn: (message: any) => VercelRSCMessage;
+      };
+    }
+  | undefined;
+
+export const RSCDisplay: TextContentPartComponent = () => {
+  const convertFn = useThread((t) => {
+    const extras = (t.extras as RSCThreadExtras)?.[symbolInternalRSCExtras];
+    if (!extras)
+      throw new Error(
+        "This function can only be used inside a Vercel RSC runtime.",
+      );
+    return extras.convertFn;
+  });
+  return useMessage((m) => convertFn(getExternalStoreMessage(m)).display);
+};
 
 const vercelToThreadMessage = <T,>(
   converter: (message: T) => VercelRSCMessage,
@@ -28,20 +56,37 @@ export const useVercelRSCRuntime = <T extends WeakKey>(
   const onNew = adapter.onNew;
   if (!onNew)
     throw new Error("You must pass a onNew function to useVercelRSCRuntime");
-  const eAdapter: ExternalStoreAdapter<any> = {
-    isRunning: adapter.isRunning,
+
+  const convertFn = useMemo(() => {
+    return (
+      adapter.convertMessage?.bind(adapter) ?? ((m: T) => m as VercelRSCMessage)
+    );
+  }, [adapter.convertMessage]);
+  const callback = useCallback(
+    (m: T) => {
+      return vercelToThreadMessage(convertFn, m);
+    },
+    [convertFn],
+  );
+
+  const messages = useExternalMessageConverter({
+    callback,
+    isRunning: adapter.isRunning ?? false,
     messages: adapter.messages,
+  });
+
+  const eAdapter: ExternalStoreAdapter = {
+    isRunning: adapter.isRunning,
+    messages,
     onNew,
     onEdit: adapter.onEdit,
     onReload: adapter.onReload,
-    convertMessage: (m: T) =>
-      vercelToThreadMessage(
-        adapter.convertMessage ?? ((m) => m as VercelRSCMessage),
-        m,
-      ),
     adapters: adapter.adapters,
     unstable_capabilities: {
       copy: false,
+    },
+    extras: {
+      [symbolInternalRSCExtras]: { convertFn },
     },
   };
 
