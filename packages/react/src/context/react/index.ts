c668ffa12fe1cdb15e3a8a03787803658db6aeba
Simon Farshid <simon.farshid@outlook.com>
2025-01-12 00:19:34 -0800

    more fixes

--- packages/react/src/context/react/index.ts
+++ packages/react/src/context/react/index.ts
@@ -1,3 +1,7 @@
+"use client";
+// TODO createContextStoreHook does not work well with server-side nextjs bundler
+// use client necessary here for now
+
 export {
   useAssistantRuntime,
   useThreadList,
--- packages/react/src/utils/smooth/index.ts
+++ packages/react/src/utils/smooth/index.ts
@@ -1,2 +1,6 @@
+"use client";
+// TODO createContextStoreHook does not work well with server-side nextjs bundler
+// use client necessary here for now
+
 export { useSmooth } from "./useSmooth";
 export { useSmoothStatus, withSmoothContextProvider } from "./SmoothContext";
