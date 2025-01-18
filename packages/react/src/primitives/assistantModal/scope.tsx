30f12d44fe32140f00d069e55641797be4a6651e
Simon Farshid <simon.farshid@outlook.com>
2025-01-17 17:47:06 -0800

    refactor: cleanup package dependencies

--- packages/react/src/primitives/assistantModal/scope.tsx
+++ packages/react/src/primitives/assistantModal/scope.tsx
@@ -1,5 +1,6 @@
 import * as PopoverPrimitive from "@radix-ui/react-popover";
-import type { Scope } from "@radix-ui/react-context";
 
-export type ScopedProps<P> = P & { __scopeAssistantModal?: Scope };
 export const usePopoverScope = PopoverPrimitive.createPopoverScope();
+export type ScopedProps<P> = P & {
+  __scopeAssistantModal?: Parameters<typeof usePopoverScope>[0];
+};
