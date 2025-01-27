7f8c27e1b2de351464585e594c38af8f745710a3
Simon Farshid <simon.farshid@outlook.com>
2025-01-26 23:03:34 -0800

    fix: useInputSync IME composition support (#1446)

---
+++ .changeset/cold-ladybugs-sniff.md
@@ -0,0 +1,5 @@
+---
+"@assistant-ui/react-ai-sdk": patch
+---
+
+fix: useInputSync IME composition support
--- packages/react-ai-sdk/src/ui/utils/useInputSync.tsx
+++ packages/react-ai-sdk/src/ui/utils/useInputSync.tsx
@@ -1,31 +1,24 @@
 import { useEffect } from "react";
 import { useAssistant, useChat } from "@ai-sdk/react";
 import { AssistantRuntime } from "@assistant-ui/react";
-import { useCallbackRef } from "@radix-ui/react-use-callback-ref";
 
 type VercelHelpers =
   | ReturnType<typeof useChat>
   | ReturnType<typeof useAssistant>;
 
 export const useInputSync = (
-  helpers: VercelHelpers,
+  { setInput, input }: VercelHelpers,
   runtime: AssistantRuntime,
 ) => {
   // sync input from vercel to assistant-ui
   useEffect(() => {
-    if (runtime.thread.composer.getState().text !== helpers.input) {
-      runtime.thread.composer.setText(helpers.input);
-    }
-  }, [helpers, runtime]);
+    runtime.thread.composer.setText(input);
+  }, [runtime, input]);
 
   // sync input from assistant-ui to vercel
-  const handleThreadUpdate = useCallbackRef(() => {
-    if (runtime.thread.composer.getState().text !== helpers.input) {
-      helpers.setInput(runtime.thread.composer.getState().text);
-    }
-  });
-
   useEffect(() => {
-    return runtime.thread.subscribe(handleThreadUpdate);
-  }, [runtime, handleThreadUpdate]);
+    return runtime.thread.composer.subscribe(() => {
+      setInput(runtime.thread.composer.getState().text);
+    });
+  }, [runtime, setInput]);
 };
