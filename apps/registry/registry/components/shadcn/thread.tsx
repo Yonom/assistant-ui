52ea2bd5907afc5b6c2eeadf38a167bd21e783fc
Simon Farshid <simon.farshid@outlook.com>
2025-01-31 01:27:03 -0800

    feat: add scroll to bottom and cancel button to shadcn minimal thread, sync shadcn styles (#1466)

--- apps/registry/registry/components/shadcn/thread.tsx
+++ apps/registry/registry/components/shadcn/thread.tsx
@@ -6,9 +6,10 @@ import {
   ThreadPrimitive,
 } from "@assistant-ui/react";
 import type { FC } from "react";
-import { SendHorizontalIcon } from "lucide-react";
 
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { ArrowDownIcon, SendHorizontalIcon } from "lucide-react";
+import { MarkdownText } from "@/components/assistant-ui/markdown-text";
 import { TooltipIconButton } from "@/components/assistant-ui/tooltip-icon-button";
 
 export const Thread: FC = () => {
@@ -19,18 +20,34 @@ export const Thread: FC = () => {
 
         <ThreadPrimitive.Messages
           components={{
-            UserMessage: UserMessage,
-            AssistantMessage: AssistantMessage,
+            UserMessage,
+            AssistantMessage,
+            // EditComposer,
           }}
         />
 
         <div className="min-h-8 flex-grow" />
 
         <div className="sticky bottom-0 mt-3 flex w-full max-w-2xl flex-col items-center justify-end rounded-t-lg bg-inherit pb-4">
+          <ThreadScrollToBottom />
           <Composer />
         </div>
       </ThreadPrimitive.Viewport>
     </ThreadPrimitive.Root>
+  );
+};
+
+const ThreadScrollToBottom: FC = () => {
+  return (
+    <ThreadPrimitive.ScrollToBottom asChild>
+      <TooltipIconButton
+        tooltip="Scroll to bottom"
+        variant="outline"
+        className="absolute -top-8 rounded-full disabled:invisible"
+      >
+        <ArrowDownIcon />
+      </TooltipIconButton>
+    </ThreadPrimitive.ScrollToBottom>
   );
 };
 
@@ -56,15 +73,28 @@ const Composer: FC = () => {
         rows={1}
         className="placeholder:text-muted-foreground max-h-40 flex-grow resize-none border-none bg-transparent px-2 py-4 text-sm outline-none focus:ring-0 disabled:cursor-not-allowed"
       />
-      <ComposerPrimitive.Send asChild>
-        <TooltipIconButton
-          tooltip="Send"
-          variant="default"
-          className="my-2.5 size-8 p-2 transition-opacity ease-in"
-        >
-          <SendHorizontalIcon />
-        </TooltipIconButton>
-      </ComposerPrimitive.Send>
+      <ThreadPrimitive.If running={false}>
+        <ComposerPrimitive.Send asChild>
+          <TooltipIconButton
+            tooltip="Send"
+            variant="default"
+            className="my-2.5 size-8 p-2 transition-opacity ease-in"
+          >
+            <SendHorizontalIcon />
+          </TooltipIconButton>
+        </ComposerPrimitive.Send>
+      </ThreadPrimitive.If>
+      <ThreadPrimitive.If running>
+        <ComposerPrimitive.Cancel asChild>
+          <TooltipIconButton
+            tooltip="Cancel"
+            variant="default"
+            className="my-2.5 size-8 p-2 transition-opacity ease-in"
+          >
+            <CircleStopIcon />
+          </TooltipIconButton>
+        </ComposerPrimitive.Cancel>
+      </ThreadPrimitive.If>
     </ComposerPrimitive.Root>
   );
 };
@@ -72,23 +102,45 @@ const Composer: FC = () => {
 const UserMessage: FC = () => {
   return (
     <MessagePrimitive.Root className="grid w-full max-w-2xl auto-rows-auto grid-cols-[minmax(72px,1fr)_auto] gap-y-2 py-4">
+      {/* <UserActionBar /> */}
+
       <div className="bg-muted text-foreground col-start-2 row-start-1 max-w-xl break-words rounded-3xl px-5 py-2.5">
         <MessagePrimitive.Content />
       </div>
+
+      {/* <BranchPicker className="col-span-full col-start-1 row-start-2 -mr-1 justify-end" /> */}
     </MessagePrimitive.Root>
   );
 };
 
 const AssistantMessage: FC = () => {
   return (
-    <MessagePrimitive.Root className="relative grid w-full max-w-2xl grid-cols-[auto_1fr] grid-rows-[auto_1fr] py-4">
+    <MessagePrimitive.Root className="relative grid w-full max-w-2xl grid-cols-[auto_auto_1fr] grid-rows-[auto_1fr] py-4">
       <Avatar className="col-start-1 row-span-full row-start-1 mr-4">
         <AvatarFallback>A</AvatarFallback>
       </Avatar>
 
-      <div className="text-foreground col-start-2 row-start-1 my-1.5 max-w-xl break-words leading-7">
-        <MessagePrimitive.Content />
+      <div className="text-foreground col-span-2 col-start-2 row-start-1 my-1.5 max-w-xl break-words leading-7">
+        <MessagePrimitive.Content components={{ Text: MarkdownText }} />
       </div>
+
+      {/* <AssistantActionBar /> */}
+
+      {/* <BranchPicker className="col-start-2 row-start-2 -ml-2 mr-2" /> */}
     </MessagePrimitive.Root>
+  );
+};
+
+const CircleStopIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      width="16"
+      height="16"
+    >
+      <rect width="10" height="10" x="3" y="3" rx="2" />
+    </svg>
   );
 };
