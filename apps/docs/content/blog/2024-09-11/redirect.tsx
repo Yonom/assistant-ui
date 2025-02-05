657279cee81e9fe84a1e945dddc231cf56260a94
Simon Farshid <simon.farshid@outlook.com>
2025-02-04 21:26:59 -0800

    docs: add two previous blog posts (#1530)

---
+++ apps/docs/content/blog/2024-09-11/redirect.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export const Redirect = () => {
+  redirect("https://medium.com/relta/github-assistant-49ae388ad758");
+};
---
+++ apps/docs/content/blog/2024-12-15/redirect.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export const Redirect = () => {
+  redirect("https://blog.langchain.dev/assistant-ui/");
+};
