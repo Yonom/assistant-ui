cdc37e0a2aa96a2188e1970aea7385093c5eb5bf
Simon Farshid <simon.farshid@outlook.com>
2025-02-10 13:12:32 -0800

    feat(cli): assistant-ui init (#1592)
    * feat(cli): assistant-ui init
    
    * version

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -6,6 +6,7 @@ import packageJson from "../package.json" with { type: "json" };
 import { create } from "./commands/create";
 import { shadcnAdd } from "./commands/shadcn/add";
 import { codemodCommand, upgradeCommand } from "./commands/upgrade";
+import { init } from "./commands/init";
 
 process.on("SIGINT", () => process.exit(0));
 process.on("SIGTERM", () => process.exit(0));
@@ -22,6 +23,7 @@ function main() {
 
   program.addCommand(shadcnAdd);
   program.addCommand(create);
+  program.addCommand(init);
   program.addCommand(codemodCommand);
   program.addCommand(upgradeCommand);
 
