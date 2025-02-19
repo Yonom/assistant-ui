468565245e3ea492525c499266f1e59f1f00953c
Simon Farshid <simon.farshid@outlook.com>
2025-02-19 14:32:29 -0800

    feat: useToolArgsFieldStatus API (#1654)

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -7,4 +7,6 @@ export * from "./runtimes";
 export * from "./types";
 export * from "./ui";
 
+export { useToolArgsFieldStatus as unstable_useToolArgsFieldStatus } from "./utils/json/parse-partial-json";
+
 export * as INTERNAL from "./internal";
